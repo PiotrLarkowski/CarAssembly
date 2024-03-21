58932c46ce4fcd8d8881d82182d9546971edccfd
Piotr Larkowski <p.larkowski90@gmail.com>
2024-03-21 15:23:05 +0100

    Add method count how many cars have been created depending on speed

--- src/CarsAssemble.java
+++ src/CarsAssemble.java
@@ -1,3 +1,18 @@
 public class CarsAssemble {
-
+    public double productionRatePerHour(int speed) {
+        double productionRatePerHour = 0;
+        switch(speed){
+            case 1 -> productionRatePerHour=speed*221;
+            case 2 -> productionRatePerHour=speed*221;
+            case 3 -> productionRatePerHour=speed*221;
+            case 4 -> productionRatePerHour=speed*221;
+            case 5 -> productionRatePerHour=(speed*221)*0.9;
+            case 6 -> productionRatePerHour=speed*221*0.9;
+            case 7 -> productionRatePerHour=speed*221*0.9;
+            case 8 -> productionRatePerHour=speed*221*0.9;
+            case 9-> productionRatePerHour=speed*221*0.8;
+            case 10 -> productionRatePerHour=speed*221*0.77;
+        };
+        return productionRatePerHour;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         CarsAssemble carsAssemble = new CarsAssemble();
+        System.out.println(carsAssemble.productionRatePerHour(6));
     }
 }
