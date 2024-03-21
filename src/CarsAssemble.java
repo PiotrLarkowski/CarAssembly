ec8c56a7805bed234d6ec17905be518f411621ba
Piotr Larkowski <p.larkowski90@gmail.com>
2024-03-21 15:41:12 +0100

    added a method that counts how many functional cars are created per minute

--- src/CarsAssemble.java
+++ src/CarsAssemble.java
@@ -2,17 +2,28 @@ public class CarsAssemble {
     public double productionRatePerHour(int speed) {
         double productionRatePerHour = 0;
         switch(speed){
-            case 1 -> productionRatePerHour=speed*221;
-            case 2 -> productionRatePerHour=speed*221;
-            case 3 -> productionRatePerHour=speed*221;
-            case 4 -> productionRatePerHour=speed*221;
-            case 5 -> productionRatePerHour=(speed*221)*0.9;
-            case 6 -> productionRatePerHour=speed*221*0.9;
-            case 7 -> productionRatePerHour=speed*221*0.9;
-            case 8 -> productionRatePerHour=speed*221*0.9;
+            case 1, 2, 3, 4 -> productionRatePerHour=speed*221;
+            case 5, 6, 7, 8 -> productionRatePerHour=(speed*221)*0.9;
             case 9-> productionRatePerHour=speed*221*0.8;
             case 10 -> productionRatePerHour=speed*221*0.77;
         };
         return productionRatePerHour;
+    }
+    public int workingItemsPerMinute(int speed) {
+        double doubleValueOfProductionRatePerMinute = 0;
+        switch(speed){
+            case 1 -> doubleValueOfProductionRatePerMinute=(speed*221)/60;
+            case 2 -> doubleValueOfProductionRatePerMinute=(speed*221)/60;
+            case 3 -> doubleValueOfProductionRatePerMinute=(speed*221)/60;
+            case 4 -> doubleValueOfProductionRatePerMinute=(speed*221)/60;
+            case 5 -> doubleValueOfProductionRatePerMinute=(speed*221*0.9)/60;
+            case 6 -> doubleValueOfProductionRatePerMinute=(speed*221*0.9)/60;
+            case 7 -> doubleValueOfProductionRatePerMinute=(speed*221*0.9)/60;
+            case 8 -> doubleValueOfProductionRatePerMinute=(speed*221*0.9)/60;
+            case 9 -> doubleValueOfProductionRatePerMinute=(speed*221*0.8)/60;
+            case 10 -> doubleValueOfProductionRatePerMinute=(speed*221*0.77)/60;
+        };
+        int intValueOfProductionRatePerMinute = (int)doubleValueOfProductionRatePerMinute;
+        return intValueOfProductionRatePerMinute;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         CarsAssemble carsAssemble = new CarsAssemble();
         System.out.println(carsAssemble.productionRatePerHour(6));
+        System.out.println(carsAssemble.workingItemsPerMinute(6));
     }
 }
